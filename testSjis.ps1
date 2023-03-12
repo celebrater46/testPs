6560715d1d0450b3e81a7f88c36fdecfe6925976
celebrater <tkgm45nbchk@gmail.com>
2023-03-12 21:57:44 +0900

    Succeeded to type some Japanese words from PS1 with SJIS to Sakura Editor with UTF-8

--- testSjis.ps1
+++ testSjis.ps1
@@ -2,3 +2,13 @@
 $answer = [Microsoft.VisualBasic.Interaction]::InputBox("何か打ち込んで。", "SJISテスト")
 
 write-host $answer
+
+Start-Process -FilePath "C:\Program Files (x86)\sakura\sakura.exe"
+Start-Sleep -m 2000
+
+# declare .NET Framework
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+[System.Windows.Forms.SendKeys]::SendWait($answer)
+
+# Succeeded to type some Japanese words from PS1 with SJIS to Sakura Editor with UTF-8
