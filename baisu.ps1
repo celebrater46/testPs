496cd1022f75ae6e49f6a73bb49aed4df862bc0a
celebrater <tkgm45nbchk@gmail.com>
2023-03-17 06:44:02 +0900

    temp

--- baisu.ps1
+++ baisu.ps1
@@ -1,9 +1,7 @@
 for($i = 1; $i -le 100; $i++){
-    # $answer = $i % 3
     if($i % 3 -eq 0){
         Write-Host "i is multiple of 3"
     } else {
         write-host $i
-        # write-host $answer
     }
 }
