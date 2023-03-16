fdce44ab9141dd67c37b401f31b87f45dcce26c4
celebrater <tkgm45nbchk@gmail.com>
2023-03-17 06:37:28 +0900

    baisu test succeeded

--- hw.ps1
+++ hw.ps1
@@ -1,5 +1,7 @@
 # TYPE "Powershell .\hw.ps1" TO CALL THIS.
 Write-Host "HELLO WORLD!!!"
+[double] $num = 441 % 16
+Write-Host $num
 
 # Get-ExecutionPolicy checks the current PS security setting
 # Set-ExecutionPolicy changes the security setting
